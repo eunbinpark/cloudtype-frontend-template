45de3090126cb793edc63fa501ac7728fbdc106c
eunbinpark <534311@gmail.com>
2024-08-21 16:48:11 +0900

    fixed : 서비스 경로 수정

--- src/services/DrugService.js
+++ src/services/DrugService.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 
-class DrugService{
+class DrugService {
 
-    searchDrug(tki, drug){
-        return axios.get(process.env.REACT_APP_WAITLIST_API_URL + "/drug" , tki, drug)
+    searchDrug(tki, drug) {
+        return axios.get(`${process.env.REACT_APP_WAITLIST_API_URL}/drug`, {
+            params: {
+                tki: tki,
+                drug: drug
+            }
+        });
     }
-    
-    searchEfficacy(efficacySearch){
+
+    searchEfficacy(efficacySearch) {
         return axios.get(process.env.REACT_APP_WAITLIST_API_URL, efficacySearch);
     }
 }
